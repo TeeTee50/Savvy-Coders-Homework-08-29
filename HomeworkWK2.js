21c79eaa977289a3a983b03394025d97ceba67b7
LaTishia <teecookies123@gmail.com>
2022-08-29 16:49:04 -0500

    HomeworkWK2.js

---
+++ HomeworkWK2.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const [restaurant, number] = ["pizzaPlace, numberOfToppings"];
+console.log(restaurant); // "pizzaPlace"
+console.log(number); // "numberOfToppings"
+typeof restaurant; //"Pizza Parlor"
+typeof number; // "1, 2, 3,"
+`My pizza place offers 3`;
+// eslint-disable-next-line no-undef
+If(quality, notQuantity);
+{
+  //a whole lot of pizza
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Savvy-Coders-Homework-08-29
 ON my own Homework 08/29
+## LaTishia LaNier
+##### I am ambitious and I have an amazing ability to make something out of nothing
+
+* Be a Web designer 1
+* Learn Coding 2
+* Get a great paying position 3
+* Gain further knowledge in Front end web development 4
+* Gain further knowledge in Back edn web Development 5
+
+
